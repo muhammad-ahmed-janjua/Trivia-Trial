fab7b44e9a7065447480b8e03fce99651ccc38e4
muhammad-ahmed-janjua <muhammadahmedjanjua.02@gmail.com>
2024-11-08 18:24:01 +1100

    django setup

--- Backend/server/urls.py
+++ Backend/server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for core project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
